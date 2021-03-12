7656b47a09effba50dfc131140ede5b99dd3a59d
root <root@localhost.localdomain>
2021-03-12 19:44:11 -0300

    first

---
+++ src/main/java/com/CharlesGodinho/excursao/ExcursaoApplication.java
@@ -0,0 +1,13 @@
+package com.CharlesGodinho.excursao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcursaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CharlesGodinho/excursao/ExcursaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CharlesGodinho.excursao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
